406c413467feffd663bf686c9a0e3579d0fdc4dc
Paul-prog44 <saintouenfr@hotmail.com>
2024-02-15 16:57:48 +0100

    Add check password fct, fix misc

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -72,6 +72,23 @@ class UserController extends AbstractController
 
         return $content;
     }
+    //Vérififcation de la complexité du mdp
+    public function checkPassword($password) : bool
+    {
+        $requiredPoints = 10;
+        $length = strlen($password);
+        $points_length = 0;
+        $points_comp = 0;
+        if($length >=10 ) { $points_length +=1 ;}
+        if(preg_match("/[a-z]/", $password)) { $points_comp++ ;}
+        if(preg_match("/[A-Z]/", $password)) { $points_comp+= 2 ;}
+        if(preg_match("/[0-9]/", $password)) { $points_comp+=3 ;}
+        if(preg_match("/\W/", $password)) { $points_comp+=4 ;}
+
+        $results = $points_length * $points_comp;
+        return ($results === $requiredPoints);
+
+    }
 
 
     
@@ -119,7 +136,7 @@ class UserController extends AbstractController
             return $this->render('errorTemplate.html.twig', ["error" => $e]);
         }
 
-        if ($_POST["oldPassword"] === $currentPassword)
+        if ($_POST["oldPassword"] === $currentPassword &&  $this->checkPassword($_POST['newPassword']))
         {
             try { $this->httpClient->request(
                 'PUT', 'https://127.0.0.1:8000/api/patients/'.$patientId, [
@@ -174,21 +191,29 @@ class UserController extends AbstractController
     #[Route('confirmationAccountCreation', name: 'confirmationAccountCreation')]
     public function confirmationAccountCreation(Request $request): Response
     {
-        try{
+
+        if ($this->checkPassword($_POST['password']) && $_POST['password'] === $_POST['passwordConfirmation'] ){
             $user = $this->postAccountCreation();
-            $session = $request->getSession();
-            //Mise en session de l'utilisateur
-            $session->set('id', $user['id']);
-            $session->set('gender', $user['gender']);
-            $session->set('last_name', $user['lastName']);
-            $session->set('first_name', $user['firstName']);
-            $session->set('address', $user['address']);
-            $session->set('email_address', $user['emailAddress']);
-            $session->set('social_security', $user['socialSecurity']);
-            $session->set('medical_file_id', $user['medicalFile']['id']);
-            return $this->render('confirmation/confirmationAccountCreation.html.twig');
-        } catch (Exception $e) {
-            return $this->render('errorTemplate.html.twig', ["error" => $e]);
+            try{
+                $session = $request->getSession();
+                //Mise en session de l'utilisateur
+                $session->set('id', $user['id']);
+                $session->set('gender', $user['gender']);
+                $session->set('last_name', $user['lastName']);
+                $session->set('first_name', $user['firstName']);
+                $session->set('address', $user['address']);
+                $session->set('email_address', $user['emailAddress']);
+                $session->set('social_security', $user['socialSecurity']);
+                $session->set('medical_file_id', $user['medicalFile']['id']);
+                return $this->render('confirmation/confirmationAccountCreation.html.twig');
+            } catch (Exception $e) {
+                return $this->render('errorTemplate.html.twig', ["error" => $e]);
+            }
+        } else if ($this->checkPassword($_POST['password']) && $_POST['password'] !== $_POST['passwordConfirmation'] ) {
+            return $this->render('error/differentPasswords.html.twig');
+        } else {
+            return $this->render('error/passwordUnsafe.html.twig'); 
         }
+        
     }
 }
